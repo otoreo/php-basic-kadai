b5a6067ef042588d79153e6583e7cceaddbb9b41
otoreo <pinkyost123@outlook.jp>
2024-12-02 19:57:13 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+         $datas = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+        foreach($datas as $key => $value){
+            echo "{$key}：{$value}<br>";
+        }
+        ?>
+    </p>
+</body>
+</html>
